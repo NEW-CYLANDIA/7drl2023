a65adf0ab800b7d32690a9218f3c98d26f914e89
Taylor Anderson <taylor-anderson@hotmail.ca>
2023-03-05 10:37:44 -0500

    trying out press-release controls, also added some taste bud gfx

--- src/Player.gd
+++ src/Player.gd
@@ -11,13 +11,15 @@ enum State {
 @export var tongue_length:float = 100;
 @export var tongue_extend_speed = 0.5;
 @export var tongue_grapple_point_sprite:Node2D
+@export var tongue_sprite:Sprite2D;
+
+var tongue_sprite_connect_pos:Node2D;
 
 var dir = 1;
 
 var state:State = State.Moving;
 
 var tongue_ray:RayCast2D;
-var tongue_sprite:Sprite2D;
 var tongue_length_normalizer:float;
 
 var tongue_grapple_point:Vector2;
@@ -28,35 +30,26 @@ var target_vel:Vector2 = Vector2(0, 0);
 
 
 func _ready():
-	tongue_sprite = $Sprite/Tongue;
 	tongue_ray = $TongueRay;
 	tongue_length_normalizer = 1.0 / tongue_sprite.texture.get_height();
 	tongue_ray.target_position = tongue_ray.target_position.normalized() * tongue_length;
-func _physics_process(delta):
+	tongue_sprite_connect_pos = $Sprite/TonguePos;
 	
+	tongue_sprite.visible = false;
+func _physics_process(delta):
 	if (state != State.Transitioning):
 		# Add the gravity.
 		if not is_on_floor():
 			velocity.y += gravity * delta
 
 	if (state == State.Moving):
-		if (sign(dir) != sign($WallRay.target_position.x)):
-			print ($TongueGuide.rotation_degrees)
-			if (round($TongueGuide.rotation_degrees) == -135):
-				print("hallo");
-				$TongueGuide.rotation_degrees -= 90;
-			else:
-				$TongueGuide.rotation_degrees += 90;
-			$TongueRay.target_position.x *= -1;
-			$WallRay.target_position.x *= -1;
+		check_flip();
 		if Input.is_action_just_pressed("action"):
 			if (is_on_floor()):
-				velocity.y += jump_vel;
-			elif (tongue_ray.is_colliding()):
-				var collider = tongue_ray.get_collider()
-				tongue_grapple_point = tongue_ray.get_collision_point();
-				tongue_length = position.distance_to(tongue_grapple_point);
-				change_state(State.Transitioning);
+				jump();
+		if Input.is_action_just_released("action"):
+			print("hello");
+			change_state(State.Transitioning)
 		
 		# Get the input direction and handle the movement/deceleration.
 		target_vel.x = dir * speed;
@@ -76,31 +69,46 @@ func _physics_process(delta):
 			tongue_sprite.scale.y = target_tongue_length;
 			change_state(State.Swinging);
 		
-			
-		if (Input.is_action_just_released("action")):
-			change_state(State.Moving);
-		
 	if (state == State.Swinging):
-		update_tongue_visuals();
+		
 		var dir_to_grapple = (tongue_grapple_point - position).normalized();
 		var speed_towards_point = velocity.dot(dir_to_grapple) 
 		if (position.distance_to(tongue_grapple_point) > tongue_length):
 			velocity -= speed_towards_point * dir_to_grapple;
 			#position = tongue_grapple_point - dir_to_grapple * tongue_length;
-		if (Input.is_action_just_released("action")):
+		if (Input.is_action_just_pressed("action")):
 			change_state(State.Moving);
-			velocity.y += jump_vel;
+			jump();
 			dir = sign(velocity.x);
-			
+		
 
-	
 	move_and_slide()
+	update_tongue_visuals();
+	tongue_sprite.position = tongue_sprite_connect_pos.global_position;
+
+func jump():
+	velocity.y += jump_vel;
+	
+func grab_grapple_point():
+	var collider = tongue_ray.get_collider()
+	tongue_grapple_point = tongue_ray.get_collision_point();
+	tongue_length = position.distance_to(tongue_grapple_point);
+	
+func check_flip():
+	if (sign(dir) != sign($WallRay.target_position.x)):
+		$TongueRay.target_position.x *= -1;
+		$WallRay.target_position.x *= -1;
+		if (round($TongueGuide.rotation_degrees) == -135):
+			$TongueGuide.rotation_degrees -= 90;
+		else:
+			$TongueGuide.rotation_degrees += 90;
 
 func change_state(new_state:State):
 	tongue_grapple_point_sprite.visible = false;
 	if new_state == State.Moving:
-		tongue_sprite.scale.y = 0;
+		tongue_sprite.visible = false;
 	if new_state == State.Swinging:
+		tongue_sprite.visible = true;
 		var artificial_vel = (tongue_grapple_point - position);
 		artificial_vel.y *= -1;
 		artificial_vel *= 1;
@@ -111,7 +119,8 @@ func change_state(new_state:State):
 		update_tongue_visuals();
 
 	if new_state == State.Transitioning:
-		pass
+		grab_grapple_point();
+
 	
 	state = new_state;
 
@@ -119,4 +128,4 @@ func update_tongue_visuals(update_scale = true):
 	var pointing_vec = (tongue_grapple_point - position);
 	
 	if (update_scale): tongue_sprite.scale.y = pointing_vec.length() * tongue_length_normalizer;
-	tongue_sprite.rotation = pointing_vec.angle() - $Sprite.rotation + PI/2;
+	tongue_sprite.rotation = pointing_vec.angle() + PI/2;
