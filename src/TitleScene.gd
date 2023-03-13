e175a5b99af81b8fe0480cdd6ac1a3126b3e4802
Taylor Anderson <taylor-anderson@hotmail.ca>
2023-03-12 20:33:54 -0400

    fix bug with end of run menu, added title screen

--- src/TitleScene.gd
+++ src/TitleScene.gd
@@ -8,14 +8,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#$Score/ScoreNumber.text = str(($root as GameManager).score);
 	pass # Replace with function body.
-	
 
-func show():
-	$Score/ScoreNumber.text = str(ScoreManager.current_level_score);
-	$Tween.interpolate_property(self, "modulate", Color("#00000000"), Color.white, 0.3);
-	$Tween.start();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
@@ -23,6 +17,4 @@ func show():
 
 
 func _on_Button_pressed() -> void:
-	get_tree().paused = false;
-	ScoreManager.current_level_score = 0;
-	get_tree().reload_current_scene();
+	get_tree().change_scene("res://src/GameScene.tscn");
